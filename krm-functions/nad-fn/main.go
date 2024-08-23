0cdb66cbfd55cbfc2fa0554a50aacf2bdac2a944
chmrad <128817020+chmrad@users.noreply.github.com>
2024-08-23 16:50:10 +0200

    Update main.go

--- krm-functions/nad-fn/main.go
+++ krm-functions/nad-fn/main.go
@@ -1,15 +1,10 @@
 package main
-/*ttt*/
+
 import (
 	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
 	fnr "github.com/nephio-project/nephio/krm-functions/nad-fn/fn"
 	"os"
 )
-
-r := &fnr.FnR{
-	Client: nil,
-}
-
 
 func main() {
 	if err := fn.AsMain(fn.ResourceListProcessorFunc(fnr.Run)); err != nil {
